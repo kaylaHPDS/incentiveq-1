3e283ded0f2df3f24f5f774c93e547017895c23b
kaylaHPDS <kayla.backus@highpowerdata.com>
2022-10-28 15:39:05 -0500

    Edited footer copyright

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var copyrightYear = document.querySelector("#copyright-year");
 function renderCopyright () {
     const today = new Date();
     const year = today.getFullYear();
-    copyrightYear.innerHTML = "<p>Copyright &copy; " + year + " • Built by <a href='https://highpower-design.com' target='_blank'>HighPower Web Design</a></p>";
+    copyrightYear.innerHTML = "<p>&copy; Copyright " + year + " • Ebeca Classic Vines LLC, All Rights Reserved • Built by <a href='https://highpower-design.com' target='_blank'>HighPower Web Design</a></p>";
 };
 
 window.onload = renderCopyright();
