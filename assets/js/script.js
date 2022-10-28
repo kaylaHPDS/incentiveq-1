577732512144e10f155ec214a876ee28610a2fa2
kaylaHPDS <kayla.backus@highpowerdata.com>
2022-10-28 11:44:03 -0500

    Updated footer styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var copyrightYear = document.querySelector("#copyright-year");
 function renderCopyright () {
     const today = new Date();
     const year = today.getFullYear();
-    copyrightYear.innerHTML = "<p>Copyright &copy; " + year + ", Built by <a href='https://highpower-design.com' target='_blank'>HighPower Web Design</a></p>";
+    copyrightYear.innerHTML = "<p>Copyright &copy; " + year + " • Built by <a href='https://highpower-design.com' target='_blank'>HighPower Web Design</a></p>";
 };
 
 window.onload = renderCopyright();
