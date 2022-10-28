41087a9b33e45452f9b9c46b8ed3b7cb5e8905ce
HPDS Developer <hpdsdev@HPDSs-MBP.lan>
2022-10-28 10:44:34 -0500

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var copyrightYear = document.querySelector("#copyright-year");
+
+function renderCopyright () {
+    const today = new Date();
+    const year = today.getFullYear();
+    copyrightYear.innerHTML = "<p>Copyright &copy; " + year + ", Built by <a href='https://highpower-design.com' target='_blank'>HighPower Web Design</a></p>";
+};
+
+window.onload = renderCopyright();
