a466ceb4b20ea1ff623dcb3bf81d4a5e85e70fa3
kaylaHPDS <kayla.backus@highpowerdata.com>
2022-10-31 19:31:19 -0500

    Updated background, header and footer styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var copyrightYear = document.querySelector("#copyright-year");
 function renderCopyright () {
     const today = new Date();
     const year = today.getFullYear();
-    copyrightYear.innerHTML = "<p>&copy; Copyright " + year + " • Ebeca Classic Vines LLC, All Rights Reserved • Built by <a href='https://highpower-design.com' target='_blank'>HighPower Web Design</a></p>";
+    copyrightYear.innerHTML = "<p>&copy; Copyright " + year + " • Ebeca Classic Vines LLC, All Rights Reserved • <a href='./terms.html'>Terms & Conditions</a> • <a href='./privacy.html'>Privacy Policy</a> • Built by <a href='https://highpower-design.com' target='_blank'>HighPower Web Design</a></p>";
 };
 
 window.onload = renderCopyright();
